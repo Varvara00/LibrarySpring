04838cf134aadb9cea7af958b28ab3a4738afaae
Varvara00 <varvara.galy171203@gmail.com>
2023-08-08 12:12:49 +0200

    start_project

---
+++ src/main/java/varvara/com/stringmvc/models/Person.java
@@ -0,0 +1,66 @@
+package varvara.com.stringmvc.models;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Neil Alishev
+ */
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+
+    @Min(value = 1940, message = "yearOfBirth should be greater than 1940")
+    private int yearOfBirth;
+
+//    @NotEmpty(message = "Email should not be empty")
+//    @Email(message = "Email should be valid")
+//    private String email;
+
+    public Person() {
+
+    }
+
+    public Person(int id, String name, int yearOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+//        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+}
